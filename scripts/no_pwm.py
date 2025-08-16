9ca029f4130a4c5181750fdf5f0776ef51417065
PedrohvFreitas <freitasphv@gmail.com>
2025-08-16 22:13:58 +0000

    versao com pwm adicionado e com um objeto alvo

---
+++ scripts/no_pwm.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import rospy
+import RPi.GPIO as GPIO
+from std_msgs.msg import Float64
+
+PINO_ESC = 2 #numero do pino da ESC, Não sei qual é
+FREQ_PWM = 50
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PINO_ESC, GPIO.OUT)
+
+pwm_esc = GPIO.PWM(PINO_ESC, FREQ_PWM)
+pwm_esc.start(0)
+
+
+def pwm_callback(percentage_vel_motor):
+    dc_min = 0.05 #motor desligado
+    dc_max = 0.1 #motor a 100%
+
+    dc = (dc_min + ((dc_max - dc_min) * percentage_vel_motor)) * 100
+
+    pwm_esc.ChangeDutyCycle(dc)
+
+
+def desligar_motor():
+    print("\nDesligando o motor e limpando os pinos GPIO.")
+    pwm_esc.stop()
+    GPIO.cleanup()
+
+sub_motor_vel = rospy.Subscriber("/control_node", Float64,callback = pwm_callback)
+
+rospy.on_shutdown(desligar_motor)
+
+rospy.spin()
